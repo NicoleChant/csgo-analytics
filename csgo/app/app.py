982eabcff344b3d71334d2b52af4fe960a9cbe69
NicoleChant <nicolechantzi@gmail.com>
2022-09-30 22:08:28 +0300

    completed scraper; added players info per matchId; added round info per matchId; cleaned bytecode and updated Makefile

--- csgo/app/app.py
+++ csgo/app/app.py
@@ -1,20 +1,20 @@
 from fastapi import FastAPI
-from typing import Dict , Union , Optional
+from typing import Dict , Union , Optional , List
 from tensorflow.keras.models import load_model
 import pandas as pd
 from csgo.ml.preprocessing import preprocess_features
-from csgo.scraper.data_scraper import get_matches
+from csgo.scraper.data_scraper import get_matches , get_match_round_data , get_player_ids
 
 app = FastAPI()
 model_path = "../../notebooks/csgo_tf"
 app.state.model = load_model(model_path)
 
 @app.get('/')
-def home():
+def home() -> Dict[str,str]:
     return {"CSGO API": "Made by NChan"}
 
 @app.get('/matches')
-def matches(limit : Optional[int] = None):
+def matches(limit : Optional[int] = None , rank : Optional[int] = None) -> List:
     """Returns live matches"""
     matches = []
     for match in get_matches():
@@ -22,19 +22,38 @@ def matches(limit : Optional[int] = None):
 
         if limit and len(matches) >= limit:
             break
-    return matches
+
+    return list(filter(lambda match : match.get("rankNum") == rank , matches)) if rank else matches
 
 
-@app.get('/match/{matchId}')
-def match(matchId : int):
-    return {"match": matchId}
+@app.get('/match/{matchId}/players')
+def players(matchId : int) -> Dict:
+    player_ids = get_player_ids(matchId)
+    return {"match" : matchId , "team_0" : player_ids[:5] , "team_1" : player_ids[5:]}
+
+@app.get('/players/{playerId}')
+def get_player_info(playerId : int) -> Dict:
+    return {"message" : "Under Construction!"}
+    
+
+@app.get('/match/{matchId}/rounds')
+def match(matchId : int , round_index : Optional[int] = None) -> Dict:
+    round_data = get_match_round_data(matchId)
+    try:
+        if round_index:
+            round_data = round_data[round_index]
+    except IndexError:
+        pass
+    finally:
+        return {"match" : matchId , "rounds" : round_data }
 
 
 @app.post('/predict')
-def predict(game_stats : Dict[str , Union[float,str,int,bool]]):
+def predict(game_stats : Dict[str , Union[float,str,int,bool]]) -> Dict:
     X_pred = pd.DataFrame(game_stats , index = [0])
     X_pred_trans = preprocess_features(X_pred)
     prediction = app.state.model.predict(X_pred_trans)
     return {"CT win": float(prediction[0][0]),
             "T win": 1 - float(prediction[0][0])}
     
+
--- csgo/scraper/data_scraper.py
+++ csgo/scraper/data_scraper.py
@@ -8,9 +8,9 @@ def get_matches():
     scraper = cloudscraper.create_scraper()
     base_url = 'https://csgostats.gg/match'
     html = scraper.get(base_url).text
+    soup = BeautifulSoup(html , 'html.parser')
 
     for match in soup.select('tr[class="p-row js-link"]'):
-    
         match_endpoint = re.findall(r'/match/(\d+)' , match.attrs['onclick'])[0]
         rank = match.find('img' , src = re.compile(r'ranks')).attrs["alt"]
         rankNum = match.find('img' , src = re.compile(r'ranks')).attrs['src'].split('/')[-1].strip('.png')
@@ -22,3 +22,111 @@ def get_matches():
                     rankNum = rankNum,
                     date = upload_time,
                     map = maps)
+
+
+def parse_team_round_score(round_info):
+     for team_info in round_info.find_all("div" , class_ = re.compile("team-")):
+        team_assigned = team_info.attrs["class"][1].split('-')[-1]
+        team_survivors = int(team_info.text.strip())
+        team_victorious = bool(len(team_info.attrs["class"]) == 3)
+        
+        yield {"team_assigned" : team_assigned,
+               "survivors": team_survivors,
+               "victorious" : team_victorious}
+        
+        
+def parse_monetary_stats(table):
+        for val in table.find_all("div" , style = re.compile("float:left; width:16%; font-size:12px;")):
+            yield val.text.strip('\n').strip()
+          
+        
+def parse_kill_death_events(table) -> dict:
+    for event in table.find_all("div" , class_ = "tl-inner"):
+        try:
+            eventTimestamp = event.find("span" , title = re.compile("Tick")).text
+            actors = event.find_all("span" , class_ = re.compile("team-"))
+
+            
+            weapon = event.find("img" , src = re.compile("weapons")).attrs["title"]
+
+            killers = list(map(lambda x : x.text , actors[:-1]))
+            deceased = actors[-1].text
+
+            is_headshot = event.find("img" , alt = "Headshot")
+            wallbang = event.find("img" , alt = "Wallbang")
+
+            yield {"eventTimestamp" : eventTimestamp,
+                    "weapon" : weapon,
+                    "killers" : killers,
+                    "deceased": deceased,
+                    "headshot" : bool(is_headshot),
+                    "wallbang" : bool(wallbang)
+                  }
+        except AttributeError as e:
+            yield {"eventTimestamp" : eventTimestamp,
+                   "weapon" : "world",
+                    "deceased" : actors[0].text
+                    }
+            
+        
+def parse_match_rounds(soup) -> dict:
+    for idx , round_ in enumerate(soup.find_all("div" , style = re.compile("padding:7px 0;"))):
+        cur_round = {f"round_{idx+1}" : {"team_0" : {} , "team_1" : {} , "kills" : []} }
+        round_info = round_.find("div" , class_ = "round-score")
+        gen = parse_team_round_score(round_info)
+        cur_round[f"round_{idx+1}"]["team_0"] = next(gen)
+        cur_round[f"round_{idx+1}"]["team_1"] = next(gen)
+
+        #print(len(round_.find_all("div" , class_ = re.compile("round-info-side"))))
+
+        monetary_table , kill_death_table = round_.find_all("div" , class_ = "round-info-side")
+
+        ## Parsing monetary table
+        gen = parse_monetary_stats(monetary_table)
+
+        ##Equipment Value
+        cur_round[f"round_{idx+1}"]["team_0"].update({"equipmentValue" : next(gen) })
+        cur_round[f"round_{idx+1}"]["team_1"].update({"equipmentValue" : next(gen) })
+
+        ##Cash
+        cur_round[f"round_{idx+1}"]["team_0"].update({"cash" : next(gen)})
+        cur_round[f"round_{idx+1}"]["team_1"].update({"cash" : next(gen)})
+
+        ##Cash Spent
+        cur_round[f"round_{idx+1}"]["team_0"].update({"cashSpent" : next(gen)})
+        cur_round[f"round_{idx+1}"]["team_1"].update({"cashSpent" : next(gen)})
+
+        ##Parsing Kill/Death tables
+        gen = parse_kill_death_events(kill_death_table)
+
+        for event in gen:
+            cur_round[f"round_{idx+1}"]["kills"].append(event)
+        yield cur_round
+
+    
+
+def get_match_round_data(matchId : int):
+    scraper = cloudscraper.create_scraper()
+    base_url = 'https://csgostats.gg/match'
+
+    html = scraper.get( base_url + f"/{matchId}#/rounds").text
+    soup = BeautifulSoup(html , 'html.parser')
+
+    return [round_ for round_ in parse_match_rounds(soup)]
+
+def parse_player_ids(soup) -> dict:
+    for player in soup.find_all(lambda tag : tag.name == "a" \
+                            and tag.has_attr("href") \
+                            and tag.has_attr("class") \
+                            and "player-link" in tag.attrs["class"] \
+                            and "player" in tag.attrs["href"]):
+        yield { player.attrs["href"].split('/')[-1] : player.text.strip('\n') }
+
+def get_player_ids(matchId : int):
+    scraper = cloudscraper.create_scraper()
+    base_url = 'https://csgostats.gg/match'
+
+    html = scraper.get( base_url + f"/{matchId}").text
+    soup = BeautifulSoup(html , 'html.parser')
+    return [player for player in parse_player_ids(soup)]
+
